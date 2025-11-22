3e232a28dfa81aed5cdf34b594cb32853a577bc4
Harshit Joshi <joshiharshit0710@gmail.com>
2025-11-22 23:30:48 +0530

    chore: commit local changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.auth.routes import router as auth_router
+from app.models import User
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "FastAPI backend is running!"}
+app.include_router(auth_router)
---
+++ backend/app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+
+class SignupSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+class ForgotPasswordSchema(BaseModel):
+    email: EmailStr
+
+class VerifyOtpSchema(BaseModel):
+    email: EmailStr
+    otp: str
